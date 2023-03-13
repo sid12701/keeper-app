437cb9cb22a46aaad262c9ef382d4bf4cc7b0d8a
sid12701 <siddhantdaryanani@gmail.com>
2023-03-13 21:06:02 +0530

    deleted:    public/favicon.ico  	modified:   public/index.html  	deleted:    public/logo192.png  	deleted:    public/logo512.png  	deleted:    public/manifest.json  	deleted:    public/robots.txt  	new file:   public/styles.css  	deleted:    src/App.css  	deleted:    src/App.js  	deleted:    src/App.test.js  	new file:   src/components/App.jsx  	new file:   src/components/Footer.jsx  	new file:   src/components/Header.jsx  	new file:   src/components/Note.jsx  	deleted:    src/index.css  	deleted:    src/logo.svg  	new file:   src/notes.js  	deleted:    src/reportWebVitals.js  	deleted:    src/setupTests.js

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(noteitem => (<Note
+        key={noteitem.key}
+        title={noteitem.title}
+        content={noteitem.content}
+      />)) }
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
